9f2fe5467f2f883011087aed3f69c737fd6b353d
McCabe Northup <mccabenorthup@gmail.com>
2019-12-15 11:32:27 -0700

    added timer elemnet to page.(Functional), Currently does not react with start button.

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     ///etc.
-  ];
+];
+
+var timerEl = document.querySelector(".timer");
+
+// Timer functon and variables
+var secondsLeft = 60; // setting timer
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = "Seconds Left: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+
+    }, 1000);
+}
+
+// function sendMessage() {
+//     timeEl.textContent = " ";
+  
+//     var imgEl = document.createElement("img");
+  
+//     imgEl.setAttribute("src", "images/image_1.jpg");
+//     mainEl.appendChild(imgEl);
+  
+//   }
+  
+  setTime();
+
